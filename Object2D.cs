882ec4ad9eaa3a74917661529c80b16463ad2f1b
RadimRiha <radim.rihacek@seznam.cz>
2021-03-25 14:19:11 +0100

    CompareTo method fix

--- Object2D.cs
+++ Object2D.cs
@@ -11,7 +11,9 @@ namespace OOP_cv7
         public abstract double Area();
         public int CompareTo(object obj)
         {
-            if (!(obj is Object2D)) throw new Exception("Cannot compare two different objects");
+            if (obj == null) return 1;
+            if (!(obj is Object2D)) throw new ArgumentException("Object is not a 2D object");
+
             if (Area() == ((Object2D)obj).Area()) return 0;
             else if (Area() > ((Object2D)obj).Area()) return 1;
             else return -1;
